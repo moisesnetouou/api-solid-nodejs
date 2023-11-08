1b499b5a37fd789100bcb4bed365f877c89a22cf
Moisés <moisesnetored@gmail.com>
2023-11-08 09:20:39 -0400

    feat: handling use case errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { RegisterUseCase } from "@/use-cases/register";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -17,8 +18,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerUseCase = new RegisterUseCase(usersRepository);
 
     await registerUseCase.execute({ email, name, password });
-  } catch (error) {
-    return reply.status(409).send();
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send();
+    }
+
+    return reply.status(500).send();
   }
 
   return reply.status(201).send();
